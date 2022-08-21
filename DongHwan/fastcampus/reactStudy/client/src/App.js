2aa4545c360b871d9f0fcbbc11263779d927c512
alazad <alazad7547@gmail.com>
2022-08-21 15:19:16 +0900

    R019
    props Boolean으로 사용하기

--- DongHwan/fastcampus/reactStudy/client/src/App.js
+++ DongHwan/fastcampus/reactStudy/client/src/App.js
@@ -15,21 +15,16 @@ import './App.css';
 //import Map from './R015_Map'
 //import Jquery from './R016_Jquery'
 //import Props from './R017_Props'
-import PropsDatatype from './R018_PropsDatatype'
+//import PropsDatatype from './R018_PropsDatatype'
+import PropsBoolean from './R019_PropsBoolean'
 
 function App() {
     return (
         <div>
             <h1>Start React 200!</h1>
             <p>CSS 적용하기</p>
-            <PropsDatatype
-             String ='react'
-             Number={200}
-             Boolean={1==1}
-             Array={[0,1,8]}
-             ObjectJson={{react:"리액트", twohundred:"200"}}
-             Function={console.log("FunctionProps: function!")}
-            />
+            <PropsBoolean BooleanTrueFalse={false}/>
+            <PropsBoolean BooleanTrueFalse/>
         </div>
     );
 }
