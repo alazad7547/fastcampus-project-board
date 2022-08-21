44c85472fc12a66da3ba86bc0ad1cb95db6e45f2
alazad <alazad7547@gmail.com>
2022-08-21 13:46:24 +0900

    R015
    map() 함수 사용하기

---
+++ DongHwan/fastcampus/reactStudy/client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+//import ImportConponent from './R003_ImportConponent'
+//import LifeCycleEx from './R004_LifeCycleEx'
+//import LifeCycleEx2 from './R005_LifeCycleEx'
+//import LifeCycleEx from './R006_LifeCycleEx'
+//import LifeCycleEx from './R007_LifeCycleEx'
+//import LifeCycleEx from './R008_LifeCycleEx'
+//import Es6 from './R009_Es6'
+//import Variable from './R010_Variable'
+//import SpreadOperator from './R011_SpreadOperator'
+//import ClassProtoType from './R012_Class&Prototype'
+//import ArrowFunc from './R013_ArrowFunction'
+//import ForEach from './R014_ForEach'
+import Map from './R015_Map'
+
+function App() {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>CSS 적용하기</p>
+            <Map/>
+        </div>
+    );
+}
+
+export default App;
