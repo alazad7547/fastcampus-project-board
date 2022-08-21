07a0f7f3f166eab97150c0d7a1ae03161a0ee990
alazad <alazad7547@gmail.com>
2022-08-21 15:03:18 +0900

    R018
    props 자료형 선언하기

--- DongHwan/fastcampus/reactStudy/client/src/App.js
+++ DongHwan/fastcampus/reactStudy/client/src/App.js
@@ -14,14 +14,22 @@ import './App.css';
 //import ForEach from './R014_ForEach'
 //import Map from './R015_Map'
 //import Jquery from './R016_Jquery'
-import Props from './R017_Props'
+//import Props from './R017_Props'
+import PropsDatatype from './R018_PropsDatatype'
 
 function App() {
     return (
         <div>
             <h1>Start React 200!</h1>
             <p>CSS 적용하기</p>
-            <Props props_val = "THIS IS PROPS"/>
+            <PropsDatatype
+             String ='react'
+             Number={200}
+             Boolean={1==1}
+             Array={[0,1,8]}
+             ObjectJson={{react:"리액트", twohundred:"200"}}
+             Function={console.log("FunctionProps: function!")}
+            />
         </div>
     );
 }
